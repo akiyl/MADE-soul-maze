52c96b345c134e0d9df3f215f4853f3c66885606
INFLAMER07 <akshatthapliyal2004@gmail.com>
2022-08-28 10:17:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const navMenu = document.querySelector('.nav_btn')
+const semi = document.querySelector('#semi')
+const content = document.querySelector('#nav_content')
+const polygon = document.querySelector('#polygon')
+const ellipse1 = document.querySelector('#path_elp1')
+const ellipse2 = document.querySelector('#path_elp2')
+const ellipse3 = document.querySelector('#path_elp3')
+const path = document.querySelector('#path_poly')
+navMenu.addEventListener('click', () => {
+
+    content.classList.toggle('hide')
+    if (path.classList === 'red') {
+        path.classList.remove('red')
+    }
+    path.classList.toggle('red')
+    ellipse1.classList.toggle('white')
+    ellipse2.classList.toggle('white')
+    ellipse3.classList.toggle('white')
+
+})
